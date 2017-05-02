3094685667bc01b1d73005a2e70305cf0e1de90e
何坦宏 <hetanhong@mofanghr.com>
2017-05-02 16:22:41 +0800

    answer result

--- routes/answer.js
+++ routes/answer.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../knexAction');
 /*middleware login request*/
 router.use((req, res, next) => {
   let sesstion = req.session;
@@ -14,8 +15,49 @@ router.use((req, res, next) => {
   }
 
 });
-router.post('/evaluation',(req, res, next)=>{
+router.post('/evaluation', (req, res, next) => {
   let param = req.body;
+  let {answer_to, result, questionnaire_id, type} = param;
+  let answer_by = req.session.user.id;
+  if (!answer_to || !result || !questionnaire_id || !type) {
+    res.status(400);
+    res.send({
+      code: 400,
+      msg: 'bad request'
+    });
+  }
+  let insertAnswer = {answer_to, answer_by, questionnaire_id, type};
+  insertAnswer.result = JSON.stringify(result);
+  knex('answer')
+          .select()
+          .where({answer_to, answer_by})
+          .then(data => {
+            if (data && data.length) {
+              return knex('answer')
+                      .where({answer_to, answer_by})
+                      .update(insertAnswer)
+            } else {
+              return knex('answer')
+                      .insert(insertAnswer);
+            }
+
+          })
+          .then((data) => {
+            res.send({
+              code: 200,
+              msg: 'ok',
+              data
+            });
+          })
+          .catch(err => {
+            res.status(500);
+            res.send({
+              code: 500,
+              msg: 'error',
+              err
+            })
+          });
+
 
 });
 module.exports = router;
