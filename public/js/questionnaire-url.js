9894beeadf11886239e6a730d74d295f75cbe355
何坦宏 <hetanhong@mofanghr.com>
2017-05-04 20:02:31 +0800

    copy url

--- public/js/questionnaire-url.js
+++ public/js/questionnaire-url.js
@@ -17,8 +17,30 @@ var vm = new Vue({
     });
 
   },
-  mounted:function () {
-    console.log(12345);
-    new Clipboard('.copy');
+  methods: {
+    copy: function (text) {
+      var dom = document.querySelector('#copy');
+      dom.setAttribute('data-clipboard-text', text);
+      dom.click();
+
+    }
+  },
+  mounted: function () {
+    var clipboard = new Clipboard('#copy');
+    clipboard.on('success', function (e) {
+      console.info('Action:', e.action);
+      console.info('Text:', e.text);
+      console.info('Trigger:', e.trigger);
+      alert('已复制成功')
+
+      e.clearSelection();
+    });
+
+    clipboard.on('error', function (e) {
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+      alert('复制失败');
+    });
+
   }
 })
